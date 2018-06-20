0b8687090d434c1a071c99d9d29d9e82294ccfe5
Mark Johnson <mjohnson24@comcast.net>
2018-06-19 22:24:54 -0500

    changed startSpeed of allEnemies

--- js/app.js
+++ js/app.js
@@ -83,7 +83,7 @@ let allEnemies = [];
 let player = new Player(200, 400);
 
 for (let i = 0; i < 3; i++) {
-    let startSpeed = 40 * Math.floor(Math.random() * 10 + 1);
+    let startSpeed = 40 * Math.floor(Math.random() * 14 + 1);
     allEnemies.push(new Enemy(-100, 65 + (85 * i), startSpeed));
 }
 
