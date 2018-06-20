1fec1f1086c258299ece285fc50a0cdcfc1f0a4f
Mark Johnson <mjohnson24@comcast.net>
2018-06-19 20:38:40 -0500

    added render method to draw player on to the screen

--- js/app.js
+++ js/app.js
@@ -58,6 +58,10 @@ Player.prototype.update = function() {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 
 // This listens for key presses and sends the keys to your
