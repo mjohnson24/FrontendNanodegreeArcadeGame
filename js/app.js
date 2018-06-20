733cf9074ce49a8ae31fc164db43267027cc1e85
Mark Johnson <mjohnson24@comcast.net>
2018-06-19 21:16:25 -0500

    changed enemy update method parameters fo this.x

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed * dt;
-    if(this.x > 505) {
-        this.x = -200;
+    if(this.x > 500) {
+        this.x = -100;
     }
 };
 
