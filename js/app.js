d49a317df4d0f9534cbe0354a1f043f2e78fee74
Mark Johnson <mjohnson24@comcast.net>
2018-06-22 16:37:32 -0500

    added player.sprite to select a new character from list of images

--- js/app.js
+++ js/app.js
@@ -32,6 +32,7 @@ for (let i = 0; i < charList.length; i++) {
     */
 ul.addEventListener('click', function(e) {
     playerCharacter = e.target.parentElement.className;
+    player.sprite = 'images/' + playerCharacter + '.png';
 });
 
 /**
