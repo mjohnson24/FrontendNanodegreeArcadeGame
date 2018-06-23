d85d137f8f0e216db6a53e0eabe197754bfd3442
Mark Johnson <mjohnson24@comcast.net>
2018-06-23 16:08:51 -0500

    changed player.playerReset inside collision to gameResaet()

--- js/app.js
+++ js/app.js
@@ -95,7 +95,7 @@ Enemy.prototype.update = function (dt) {
 
     // check for collision betweeen the player and the enemy
     if (player.x < this.x + 38 && player.x + 38 > this.x && player.y < this.y + 20 && 35 + player.y > this.y) {
-        player.playerReset();
+        player.gameReset();
     }
 };
 
